258e6282964bc80f11e6ba5a8bcbdd8e360fa4eb
Veronica Harris <veronica.n.harris92@gmail.com>
2021-10-05 18:15:25 -0400

    added colors to the boxes and divided the components from the regular src folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
 import PasswordBody from "./components/PasswordBody";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
       <PasswordBody />
     </div>
   );
---
+++ src/components/PasswordBody.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pass.css";
+
+function PasswordBody() {
+    return (
+        <div className="passwordBox">
+            <h1>Password Generator Goes Here</h1>
+        </div>
+    );
+}
+
+export default PasswordBody;
