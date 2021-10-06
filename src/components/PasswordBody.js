0632b41e5a9d0ea543b6192509a1254d28b33549
Veronica Harris <veronica.n.harris92@gmail.com>
2021-10-06 12:58:26 -0400

    working on the box styling

--- src/components/PasswordBody.js
+++ src/components/PasswordBody.js
@@ -4,7 +4,16 @@ import "./pass.css";
 function PasswordBody() {
     return (
         <div className="passwordBox">
-            <h1>Password Generator Goes Here</h1>
+            <h1>React Password Generator</h1>
+            <div className="cardHeader">
+                <h2>Generate a Password</h2>
+            </div>
+            <div className="cardBody">
+                <textarea readOnly id="password" placeholder="Your Secure Password" aria-label="Generate Password"></textarea>
+            </div>
+            <div class="cardFooter">
+                <button id="generate" className="btn">Generate Password</button>
+            </div>
         </div>
     );
 }
